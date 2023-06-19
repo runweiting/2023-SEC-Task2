7f118aeda42a96b6baa3db37d1f70ff3effdff51
runweiting <whatisfuntime@gmail.com>
2023-06-20 07:36:05 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+    $('navbar-btn').click(function(){
+        $('navbar-menu-collapse').slideToggle('show');
+    })
+
     $('.backtotop').click(function(event){
         $('body').animate({
             scrollTop:0
