817e3df2b65863e6067fb2c68c97f65bc7e3f574
runweiting <whatisfuntime@gmail.com>
2023-06-26 22:13:20 +0800

    rewrite swiper

--- js/all.js
+++ js/all.js
@@ -1,12 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('navbar-btn').click(function(){
+    $('navbar-btn').click(function () {
         $('navbar-menu-collapse').slideToggle('show');
-    })
+    });
 
-    $('.backtotop').click(function(event){
-        $('body').animate({
-            scrollTop:0
-        },800)
-    })
+    $('.backtotop').click(function (event) {
+        $('body').animate({ scrollTop: 0 },800);
+    });
+
+
+});
+
+const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 1,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        slidesPerGroup: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerGroup: 24,
+      },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
 });
