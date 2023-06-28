19bdf3d5538bfed01697ee83e2ba72d8a05cca12
runweiting <whatisfuntime@gmail.com>
2023-06-28 16:24:15 +0800

    rewrite tool filter, sort and select

--- js/all.js
+++ js/all.js
@@ -1,8 +1,34 @@
 $(document).ready(function () {
 
-  $('.navbar-btn').click(function (e) {
+  /*$('.navbar-btn').click(function (e) {
     $('.navbar-menu-collapse').slideToggle('show');
+  });*/
+  
+  /*
+  當.select-btn點擊時.dropdown-menu出現
+  當.new2old點擊時,取消事件a,使.dropdown-menu出現,使dropdown-btn-text文字變成由新到舊
+  */
+  $('.select-new-old .select-btn').click(function (e) {
+    $('.dropdown-menu').toggleClass('show');
   });
+
+  $('.new2old').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btn-text').text('由新到舊');
+  });
+
+  $('.old2new').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btn-text').text('由舊到新');
+  });
+
+
+    
+  
+
+
 
   $('.backtotop').click(function (event) {
     $('body').animate({ scrollTop: 0 },800);
